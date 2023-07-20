2e858e3838e3d119abc3fa9e8dc4c1b2df1f7935
benmneb <62878664+benmneb@users.noreply.github.com>
2023-07-20 20:30:58 +0800

    chore: init pwa

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,10 @@
+import ReloadPrompt from './reload-prompt'
+
 export default function App() {
-  return <div className="font-bold">Let's do this</div>
+  return (
+    <>
+      <div className="font-bold">Let's do this</div>
+      <ReloadPrompt />
+    </>
+  )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,51 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      // https://vite-pwa-org.netlify.app/guide/pwa-minimal-requirements.html#web-app-manifest
+      includeAssets: [
+        'favicon.ico',
+        'apple-touch-icon.png',
+        'maskable-icon-512x512.png',
+      ],
+      manifest: {
+        name: 'benmneb/portfolio',
+        short_name: 'benmneb',
+        description: 'Not just another React dev.',
+        start_url: '/?ref=pwa',
+        display: 'standalone',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: 'maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+    }),
+  ],
 })
